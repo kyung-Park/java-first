3906057bfcda06952c7fa28f22825de491b45240
kyung-Park <ruddnjs5816@naver.com>
2023-12-02 18:38:11 +0900

    231002 java-first-study

---
+++ src/CommentJava.java
@@ -0,0 +1,13 @@
+public class CommentJava {
+
+    public static void main(String[] args) {
+        System.out.println("hello java1"); //hello java1을 출력 합니다.
+        //System.out.println("hello java2");
+
+        /*
+        System.out.println("hello java");
+        System.out.println("hello java");
+     */
+    }
+
+}
---
+++ src/variable/ex/Var3.java
@@ -0,0 +1,12 @@
+package variable.ex;
+
+public class Var3 {
+
+    public static void main(String[] args) {
+        long num = 10000000000L;
+        boolean tf = true;
+
+        System.out.println("tf = " + tf);
+        System.out.println("num = " + num);
+    }
+}
